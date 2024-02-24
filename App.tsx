f7e503732f667cfb6e63f634eecd87796614b445
Thiago <prattithiago5@gmail.com>
2024-02-24 17:30:27 -0300

    PROJECT IGNITE GYM

---
+++ App.tsx
@@ -0,0 +1,33 @@
+
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+export default function App() {
+
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+
+  return (
+    <View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
+
+      {
+        fontsLoaded ? 
+        
+          <Text>Hello World</Text>
+        
+        :
+        <View/>
+      }
+      
+    </View>
+  );
+}
+
