62cbddb12fc5091b2c2dd35318c2c84795c60dfa
Thiago <prattithiago5@gmail.com>
2024-02-27 10:03:04 -0300

    CREATE GROUP

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,12 +1,38 @@
-import { Center, Text, VStack } from "native-base";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
 
 import { Header } from "@components/Home/Header";
+import { Group } from "@components/Home/Group";
+import { useState } from "react";
 
 
 export function Home(){
+
+    const [ groups ] = useState(["costas", "ombro" , "perna" , "peito" , "triceps" , "biceps"]);
+    const [ groupSelectd, setGroupSelectd ] = useState("costas")
+
     return(
         <VStack>
             <Header/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={ item => item }
+                horizontal
+
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={ groupSelectd === item }
+                        onPress={() => setGroupSelectd(item)}
+                    />
+                )}
+
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
+            
         </VStack>
     )
 }
